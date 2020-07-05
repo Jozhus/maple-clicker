40451db006633a9cec92f6ca7d23a3b8c2cc78e6
Jozhus <vauit3@yahoo.com>
2020-07-04 18:33:38 -0700

    reactapp setup

---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+import React, { Component } from "react";
+
+class App extends Component {
+
+  constructor(props: {}) {
+    super(props);
+  }
+
+  public render() {
+    return (
+      <React.Fragment>
+        <div>yep</div>
+      </React.Fragment>
+    );
+  }
+}
+
+export { App };
